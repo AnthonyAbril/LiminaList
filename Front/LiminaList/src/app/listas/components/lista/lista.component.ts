440122908cceca0b676a0c4191c2f0e499fb7bf8
Tony Abril <antabrrue@alu.edu.gva.es>
2025-03-15 11:57:33 +0100

    boceto prefuncional

--- Front/LiminaList/src/app/listas/components/lista/lista.component.ts
+++ Front/LiminaList/src/app/listas/components/lista/lista.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './lista.component.css'
 })
 export class ListaComponent {
-  
+  estados = ['Opción 1', 'Opción 2', 'Opción 3'];
+  estadoActual = 0;
+
+  toggleState() {
+    this.estadoActual = (this.estadoActual + 1) % this.estados.length;
+  }
 }
