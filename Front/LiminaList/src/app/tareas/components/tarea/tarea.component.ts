390ecc49c30c840552dc4b8dd2711b0a52b2b190
Tony Abril <antabrrue@alu.edu.gva.es>
2025-04-02 21:39:26 +0200

    intento de colores por niveles, fallas presentes

--- Front/LiminaList/src/app/tareas/components/tarea/tarea.component.ts
+++ Front/LiminaList/src/app/tareas/components/tarea/tarea.component.ts
@@ -10,5 +10,12 @@ export class TareaComponent {
   @Input() nombre!: string;
   @Input() progreso!: string;
   @Input() subtareas: any[] = [];
+  @Input() nivel: number = 0;
+
+  // Método para calcular la clase del color
+  getClasePorNivel(): string {
+    const colorIndex = this.nivel % 3; // Ciclar entre 0, 1 y 2
+    return `color-${colorIndex}`;
+  }
 }
 
