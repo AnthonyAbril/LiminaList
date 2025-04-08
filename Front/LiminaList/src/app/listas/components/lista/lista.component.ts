4b2dfb0bb319f0ba4b6168cbd62ea167ea1fb0e6
Tony Abril <antabrrue@alu.edu.gva.es>
2025-04-09 01:37:26 +0200

    agregar tarea principal funcional, falta plegar subtareas de tarea

--- Front/LiminaList/src/app/listas/components/lista/lista.component.ts
+++ Front/LiminaList/src/app/listas/components/lista/lista.component.ts
@@ -12,149 +12,156 @@ export class ListaComponent {
   
   tareas = [
     {
-      nombre: 'Tarea 1',
-      progreso: '2/2',
+      nombre: 'Estudiar PAU',
+      progreso: '',
+      subtareas: []
+    },
+    {
+      nombre: 'Estudiar PAU',
+      progreso: '',
       subtareas: [
         {
-          nombre: 'Subtarea 1',
-          progreso: '2/2',
+          nombre: 'Estudiar Matematicas',
+          progreso: '',
           subtareas: [
             {
-              nombre: 'sexo 1',
-              progreso: '2/2',
-              subtareas: []
-            },
-            {
-              nombre: 'sexo gay',
-              progreso: '2/2',
+              nombre: 'Estudiar Matrices',
+              progreso: '',
               subtareas: [
                 {
-                  nombre: 'sexo anal',
-                  progreso: '2/2',
+                  nombre: 'Estudiar Ecuaciones Matriciales',
+                  progreso: '',
+                  subtareas: []
+                },
+                {
+                  nombre: 'Estudiar Rango de Matrices',
+                  progreso: '',
                   subtareas: []
                 }
               ]
+            },
+            {
+              nombre: 'Estudiar Geometria Analítica',
+              progreso: '',
+              subtareas: []
             }
           ]
         },
         {
-          nombre: 'Subtarea 2',
-          progreso: '2/2',
-          subtareas: []
-        },
-        {
-          nombre: 'Subtarea 3',
-          progreso: '2/2',
+          nombre: 'Estudiar Fisica',
+          progreso: '',
           subtareas: [
             {
-              nombre: 'Subtarea 3',
-              progreso: '2/2',
-              subtareas: [
-                {
-                  nombre: 'Subtarea 3',
-                  progreso: '2/2',
-                  subtareas: [
-                    {
-                      nombre: 'Subtarea 3',
-                      progreso: '2/2',
-                      subtareas: []
-                    },
-                    {
-                      nombre: 'Subtarea 3',
-                      progreso: '2/2',
-                      subtareas: [
-                        {
-                          nombre: 'Subtarea 3',
-                          progreso: '2/2',
-                          subtareas: []
-                        },
-                        {
-                          nombre: 'Subtarea 3',
-                          progreso: '2/2',
-                          subtareas: [
-                            {
-                              nombre: 'Subtarea 3',
-                              progreso: '2/2',
-                              subtareas: []
-                            },
-                            {
-                              nombre: 'Subtarea 3',
-                              progreso: '2/2',
-                              subtareas: []
-                            }
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                },
-                {
-                  nombre: 'Subtarea 3',
-                  progreso: '2/2',
-                  subtareas: []
-                }
-              ]
+              nombre: 'Estudiar Campo Gravitatorio',
+              progreso: '',
+              subtareas: []
             }
           ]
         }
       ]
     },
     {
-      nombre: 'Tarea 2',
-      progreso: '2/2',
-      subtareas: []
-    },
-    {
-      nombre: 'Tarea 3',
-      progreso: '2/2',
-      subtareas: [{
-        nombre: 'Tarea 2',
-        progreso: '2/2',
-        subtareas: []
-      },
-      {
-        nombre: 'Tarea 3',
-        progreso: '2/2',
-        subtareas: [{
-          nombre: 'Tarea 2',
-          progreso: '2/2',
-          subtareas: []
-        },
+      nombre: 'TFG Proyecto Liminalist',
+      progreso: '',
+      subtareas: [
         {
-          nombre: 'Tarea 3',
-          progreso: '2/2',
-          subtareas: [{
-            nombre: 'Tarea 2',
-            progreso: '2/2',
-            subtareas: []
-          },
-          {
-            nombre: 'Tarea 3',
-            progreso: '2/2',
-            subtareas: [{
-              nombre: 'Tarea 2',
-              progreso: '2/2',
+          nombre: '1ra Entrega',
+          progreso: '',
+          subtareas: [
+            {
+              nombre: 'Idea de funcionalidad y logica',
+              progreso: '',
               subtareas: []
             },
             {
-              nombre: 'Tarea 3',
-              progreso: '2/2',
-              subtareas: [{
-                nombre: 'Tarea 2',
-                progreso: '2/2',
-                subtareas: []
-              },
-              {
-                nombre: 'Tarea 3',
-                progreso: '2/2',
-                subtareas: []
-              }]
-            }]
-          }]
-        }]
-      }]
+              nombre: 'Diseño de interfaz',
+              progreso: '',
+              subtareas: []
+            },
+            {
+              nombre: 'Programacion Base',
+              progreso: '',
+              subtareas: [
+                {
+                  nombre: 'Front boceto hecho',
+                  progreso: '',
+                  subtareas: [
+                    {
+                      nombre: 'Front boceto ordenador',
+                      progreso: '',
+                      subtareas: []
+                    },
+                    {
+                      nombre: 'Front boceto movil',
+                      progreso: '',
+                      subtareas: []
+                    }
+                  ]
+                },
+                {
+                  nombre: 'Logica Programada',
+                  progreso: '',
+                  subtareas: [
+                    {
+                      nombre: 'CRUD de tareas',
+                      progreso: '',
+                      subtareas: [
+                        {
+                          nombre: 'Crear tareas',
+                          progreso: '',
+                          subtareas: []
+                        },
+                        {
+                          nombre: 'Eliminar tareas',
+                          progreso: '',
+                          subtareas: []
+                        },
+                        {
+                          nombre: 'Modificar tareas',
+                          progreso: '',
+                          subtareas: []
+                        },
+                        {
+                          nombre: 'Mover tareas',
+                          progreso: '',
+                          subtareas: []
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          nombre: '2ra Entrega',
+          progreso: '',
+          subtareas: []
+        },
+        {
+          nombre: '3ra Entrega',
+          progreso: '',
+          subtareas: []
+        }
+      ]
+    },
+    {
+      nombre: 'Practicas Empresa',
+      progreso: '',
+      subtareas: []
     }
   ];
+
+  // Método para añadir una nueva subtarea
+  agregarTarea(): void {
+    const nuevaTarea = {
+      nombre: `Tarea ${this.tareas.length + 1}`, // Nombre dinámico
+      progreso: '',
+      subtareas: [], // Las subtareas empiezan vacías
+    };
+    this.tareas.push(nuevaTarea); // Añade la nueva subtarea al array
+  }
 
   toggleState() {
     this.estadoActual = (this.estadoActual + 1) % this.estados.length;
