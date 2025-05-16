d14f384b47d55f875abba3687049c5ef2e280080
Tony Abril <antabrrue@alu.edu.gva.es>
2025-05-16 21:54:11 +0200

    sacado listador de listas fuera del modulo de listas para evitar enrutamiento cruzado

--- Front/LiminaList/src/app/app-routing.module.ts
+++ Front/LiminaList/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirigir automáticamente al login
   { path: 'login', component: LoginComponent }, // Pantalla de login
   { path: 'home', loadChildren: () => import('./principal/principal.module').then(m => m.PrincipalModule) /*, canActivate: [AuthGuard] */ }, // Carga diferida del módulo principal
-  { path: 'lists', loadChildren: () => import('./listas/listas.module').then(m => m.ListasModule) /*, canActivate: [AuthGuard] */ },
+  { path: 'lists', loadChildren: () => import('./lista-visor/lista-visor.module').then(m => m.ListaVisorModule) /*, canActivate: [AuthGuard] */ },
   { path: 'panel/:id', loadChildren: () => import('./panel/panel.module').then(m => m.PanelModule) }, // 🔹 Se agrega el parámetro dinámico `:id`
   { path: '**', redirectTo: '/login' } // Redirigir cualquier ruta desconocida al login
 ];
--- Front/LiminaList/src/app/listas/listas.module.ts
+++ Front/LiminaList/src/app/listas/listas.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListaComponent } from './components/lista/lista.component';
 import { TareasModule } from "../tareas/tareas.module";
-import { ListasComponent } from './listas.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
@@ -11,7 +10,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     ListaComponent,
-    ListasComponent
   ],
   imports: [
     CommonModule,
@@ -19,7 +17,6 @@ import { RouterModule } from '@angular/router';
     FormsModule,
     RouterModule.forChild([
       { path: 'panel/:id', component: ListaComponent }, // Ruta interna del módulo
-      { path: 'lists', component: ListasComponent }, // Ruta interna del módulo
     ])
 ],
   exports: [ListaComponent]
