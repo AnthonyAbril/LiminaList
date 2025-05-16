f5eb4f8fad6ec7d5ce65580de11a7bbd437b4e25
Tony Abril <antabrrue@alu.edu.gva.es>
2025-05-16 22:08:01 +0200

    enrutamiento cruzado solucionado, listado funcional, falta decorar el front

--- Front/LiminaList/src/app/lista-visor/lista-visor.module.ts
+++ Front/LiminaList/src/app/lista-visor/lista-visor.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListaVisorComponent } from './lista-visor.component';
 import { FormsModule } from '@angular/forms';
-import { ListaVisorRoutingModule } from './lista-visor-routing.module';
+import { RouterModule } from '@angular/router'; // ✅ importante
 
-
+import { ListaVisorComponent } from './lista-visor.component';
 
 @NgModule({
   declarations: [ListaVisorComponent],
   imports: [
     CommonModule,
     FormsModule,
-    ListaVisorRoutingModule
-  ],
-  exports: [
-    ListaVisorComponent
+    RouterModule.forChild([
+      { path: '', component: ListaVisorComponent } // ← aquí defines la ruta
+    ])
   ]
 })
 export class ListaVisorModule { }
